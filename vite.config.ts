4971da531c2bb4721337c744278df8c9dbcc4f20
deltagamingch <deltagaming.ch.nik@gmail.com>
2025-02-18 06:23:10 +0100

    refactor(build): minor indent changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+    const env = loadEnv(mode, process.cwd(), '')
 
-  return {
-    plugins: [react()],
-    build: {
-      outDir: '_dist'
-    },
-    define: {
-      'process.env': env
+    return {
+        plugins: [react()],
+        build: {
+            outDir: '_dist'
+        },
+        define: {
+            'process.env': env
+        }
     }
-  }
 })
