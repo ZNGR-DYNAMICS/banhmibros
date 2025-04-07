e4ef771273fc9bc81bc645d92044aa8cf8927055
n-zngr <deltagaming.ch.nik@gmail.com>
2025-04-07 10:10:09 +0200

    docs(layout): add function documentation
    - document function with example and purpose

--- app/contexts/LayoutScrollContext.tsx
+++ app/contexts/LayoutScrollContext.tsx
@@ -1,3 +1,11 @@
 import { LayoutScrollContext } from './LayoutScroll.context';
-
+/**
+ * @component LayoutScrollProvider
+ * @description A React Context Provider component for the LayoutScrollContext.
+ * It makes the scroll ref available to consuming components.
+ * @example
+ * <LayoutScrollProvider value={scrollRef}>
+ * {children}
+ * </LayoutScrollProvider>
+ */
 export const LayoutScrollProvider = LayoutScrollContext.Provider;
