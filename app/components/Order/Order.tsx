6916bfb10769cf247dba7b8ddbedaf4067124ce8
n-zngr <deltagaming.ch.nik@gmail.com>
2025-04-08 11:33:31 +0200

    style(order): set spacing for buttons
    - set spacing for buttons in parent order component

--- app/components/Order/Order.tsx
+++ app/components/Order/Order.tsx
@@ -12,7 +12,9 @@ const Order: React.FC = () => {
                     data-de="Bestell dein Banh Mi takeaway oder lass es direkt nach Hause liefern."
                 /> 
             </Headline>
-            <OrderButtons />
+            <div className='px-4 pb-16 md:px-8 md:pb-16 lg:px-16 lg:pb-32'>
+                <OrderButtons />
+            </div>
         </main>
     )
 }
