8baeb8efa6f55d0ac46a5ef9e9a97a1805115e69
n-zngr <deltagaming.ch.nik@gmail.com>
2025-04-09 09:42:19 +0200

    feat(main): update pass-lock, remove app-wrapper
    - remove redundant app-wrapper
    - simplify pass-lock to function, replacing useEffect and useStates
    - update production to prod-env
    - use sessionStorage, storing page-lock-state

--- app/main.tsx
+++ app/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, useState, useEffect } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import './index.css';
@@ -10,54 +10,43 @@ import Layout from './components/Layout.tsx';
 import OrderPage from './pages/OrderPage.tsx';
 import MenuPage from './pages/MenuPage.tsx';
 
-export function AppWrapper() {
-    const [isUnlocked, setIsUnlocked] = useState(
-        import.meta.env.MODE === 'prod' ? true : false
-    );
+const passwordPrompt = (): void => {
+    const validPassword = import.meta.env.VITE_PAGE_LOCK_PASS;
+    let userPassword = "";
 
-    useEffect(() => {
-        if (import.meta.env.MODE !== 'production' && !isUnlocked) {
-            const passwordPrompt = (): boolean => {
-                const validPassword = import.meta.env.VITE_PAGE_LOCK_PASS;
-                let userPassword = '';
-
-                while (userPassword !== validPassword) {
-                    userPassword = prompt('Enter the password: ') || '';
-
-                    if (userPassword === '') {
-                        alert('Access denied.');
-                        return false;
-                    } else if (userPassword !== validPassword) {
-                        alert('Incorrect password.');
-                    } else {
-                        alert('Access granted.');
-                        return true;
-                    }
-                }
-                return false;
-            };
-
-            if (passwordPrompt()) {
-                setIsUnlocked(true);
-            }
+    while (userPassword !== validPassword) {
+         userPassword = prompt('Enter the password:') || "";
+  
+        if (userPassword === "") {
+            alert("Access denied.");
+        } else if (userPassword !== validPassword) {
+            alert("Incorrect password.");
         }
-    }, [isUnlocked])
+    }
+  
+    alert("Access granted.");
+    sessionStorage.setItem('unlocked', 'true');
+};
 
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/menu" element={<Layout navColor='text-black-500'><MenuPage /></Layout>} />
-                <Route path="/legal" element={<Layout navColor='text-black-500'><Legal /></Layout>} />
-                <Route path="/order" element={<Layout navColor='text-black-500'><OrderPage /></Layout>} />
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-        </BrowserRouter>
-    )
+
+const checkPageUnlocked = (): boolean => {
+    return sessionStorage.getItem('unlocked') === 'true';
+};
+
+if (import.meta.env.MODE !== 'prod' && !checkPageUnlocked()) {
+    passwordPrompt();
 }
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <AppWrapper />
-    </StrictMode>
-)
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/menu" element={<Layout navColor='text-black-500'><MenuPage /></Layout>} />
+            <Route path="/legal" element={<Layout navColor='text-black-500'><Legal /></Layout>} />
+            <Route path="/order" element={<Layout navColor='text-black-500'><OrderPage /></Layout>} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+        </BrowserRouter>
+    </StrictMode>,
+);
