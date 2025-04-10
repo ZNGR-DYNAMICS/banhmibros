c333f7abbbb3f4c586299d7966513f5657d2fda6
n-zngr <deltagaming.ch.nik@gmail.com>
2025-04-10 18:59:36 +0200

    refactor(order): reformat styling classes for consistency

--- app/pages/OrderPage.tsx
+++ app/pages/OrderPage.tsx
@@ -7,7 +7,7 @@ const OrderPage: React.FC = () => {
 
     return (
         <div className='relative flex flex-col bg-white-500 text-black-500 pointer-events-auto'>
-            <header className='flex flex-col px-4 pt-16 md:px-8 md:pt-16 lg:px-16 gap-4'>
+            <header className='flex flex-col gap-4 px-4 pt-16 md:px-8 md:pt-16 lg:px-16'>
                 <h1 className="text-9xl circula-black break-normal" data-en="Order" data-de="Bestellen" />
             </header>
             <main>
